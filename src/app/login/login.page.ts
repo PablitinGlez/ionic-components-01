de8760a43fb50f5b02554f77edd1245c9faa31fd
Juan Pablo <gonzalez0421200@gmail.com>
2025-01-23 08:19:03 -0600

    Enlazar vista concontrolador

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,13 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class LoginPage implements OnInit {
+  //forma 1 inicialozacion directa = '',
+  //2da en el constructur acceder a this.password = ''
+  //3 decir al compilador que se encarga despues de la varaible un
+  //cuando kiero  enlazar a un evento un  ()
+  //y los corchetes
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  login() {
+    //si usaurio = admin y password = admin
+    //entonces con sole .log  login correcto
+    //si no console.log login incorrecto
+    if (this.email === 'admin' && this.password === 'admin') {
+      console.log('login correcto');
+    } else {
+      console.log('login incorrecto');
+    }
   }
-
 }
